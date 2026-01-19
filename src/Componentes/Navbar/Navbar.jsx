6306790dc878ea2a88dfa7678d32834255cde609
4DNB <davidnb03@outlook.com>
2026-01-18 21:01:57 -0500

    img

---
+++ src/Componentes/Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import './Navbar.css'
+
+function Navbar() {
+
+    return (
+        <div className="Navbar">
+            <nav>
+                <div>
+                    <li className='MainLi1'>
+                        <ul>Inicio</ul>
+                    </li>
+                </div>
+                <div>
+                    <li className='MainLi2'>
+                        <ul>Iniciar Sesion</ul>
+                    </li>
+                </div>
+            </nav>
+        </div>
+    );
+}
+
+export default Navbar;
+
+
--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,13 @@ import React, { StrictMode } from 'react';
 import ReactDOM, { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Navbar from './Componentes/Navbar/Navbar';
+import Footer from './Componentes/Footer/Footer';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Navbar />
     <App />
+    <Footer />
   </StrictMode>,
 )
